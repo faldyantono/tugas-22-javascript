3b91142eed99016deaf065c8403c514e27e8ba06
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 08:52:39 +0700

    Add files via upload

---
+++ tugas22.js
@@ -0,0 +1,7 @@
+function panggilpanggil() {
+  var data = ["Item : Saya index ke", "Item : Ingin index ke", "Item : Belajar index ke", "Item : Bersama index ke"];
+  data.forEach(function(item, array) {
+    console.log(item, array);
+  })
+}
+panggilpanggil();
